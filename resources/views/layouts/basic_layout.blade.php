66e142e0dfabb2a1c1ebbadd2d645f93dba80c37
GSG9HUN <43461831+GSG9HUN@users.noreply.github.com>
2021-10-06 18:28:23 +0200

    Basic templatek.

---
+++ resources/views/layouts/basic_layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="header">
+    @include('layouts.header.header')
+</div>
+<div class="content">
+    @yield('content')
+</div>
+<div class="footer">
+    @include('layouts.footer.footer')
+</div>
+<script href="{{asset('js/app.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/view_blades/rolunk.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.basic_layout')
+
+@section('content')
+<div id="{{\Request::route()->getName()}}">
+
+</div>
+@endsection
